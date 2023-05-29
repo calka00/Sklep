21b662d448dd8c56242bc602b9d0db8e766fb5fb
M4rtin052 <marcinpl2001@o2.pl>
2023-05-29 12:08:42 +0200

    Create CategoriesController.cs

---
+++ Sklep_komputerowy/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sklep_komputerowy.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public ActionResult Processors()
+        {
+
+            return View();
+        }
+    }
+}
