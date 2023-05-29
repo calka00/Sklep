246f5d6eda8dd23e61d0a250711a078a2fd92d65
M4rtin052 <marcinpl2001@o2.pl>
2023-05-29 12:09:13 +0200

    Create podzespoły.cs

---
+++ Sklep_komputerowy/Models/podzespoły.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sklep_komputerowy.Models
+{
+    public class podzespoły
+    {
+        public string Nazwa { get; set; }
+
+        public List<Specyfikacja> Specyfikacje { get; set; }
+
+        public string Obraz { get; set; }
+
+        public string Cena { get; set; }
+    }
+}
